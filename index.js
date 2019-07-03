f1253a799a9318b00fa9fbca915b492a744e3aaf
Aude Faucheux <aude53@gmail.com>
2019-07-03 21:02:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+var array = []
+
+function theBeatlesPlay(musicians, instruments) {
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(`${musicians.slice(i,i+1)} plays ${instruments.slice(i,i+1)}`)
+//    return array.push(`$musicians.length i} ${instruments.length i}`)
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  var i = 0
+  while (i < facts.length) {
+  `${facts.slice(i,i+1)}!!!`
+  }
+  return facts
+}
